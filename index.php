ce771c7d518713f58ea531c8f1668e27978a4997
jihad.noureddine <jihad.noureddine@tedmob.com>
2024-04-19 16:47:48 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="h1 text-center">Welcome</h1>
+        <div class="cards">
+            <div class="card">
+                <img src="./images/appache.png" alt="appache">
+                <a href="http://185.170.58.156:8088/">
+                    <h2>Apache Superset</h2>
+                </a>
+            </div>
+
+            <div class="card">
+                <img src="./images/directus.avif" alt="Directus">
+                <a href="http://185.170.58.156:8055/">
+                    <h2>Directus</h2>
+                </a>
+            </div>
+
+            <div class="card">
+                <img src="./images/qgis.webp" alt="gephi-lite">
+                <a href="http://185.170.58.156:8088/">
+                    <h2>QGIS</h2>
+                </a>
+            </div>
+       
+
+            <div class="card">
+                <img src="./images/gephi.png" alt="gephi-lite">
+                <a href="http://185.170.58.156:5173/">
+                    <h2>Gephi lite</h2>
+                </a>
+            </div>
+            
+          
+        </div>
+    </div>
+
+    <footer>
+        <p>
+            For any technical support contact 
+            <a href="mailto:jihad@gmail.com">
+                Jihad Noureddine
+            </a>
+        </p>
+    </footer>
+</body>
+</html>
