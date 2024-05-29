4203d9bbdbdac59dd592f8b0e5edefd1f743ca85
jihad.noureddine <jihad.noureddine@tedmob.com>
2024-05-29 14:33:48 +0300

    added version

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=1.0.1">
 </head>
 <body>
     <div class="container">
